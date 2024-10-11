1ceb44f55f966757a79ae0d31cb10c9b8c477c8c
Frandy Slueue <frandyslueue@hotmail.com>
2024-10-11 09:23:59 -0500

    Print a concatenated string

---
+++ python-hello_world/4-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# Complete this source code (str1 = "Holberton", str2 = "School") to print Welcome to Holberton School!
+
+# You can find the source code here
+# You are not allowed to use any loops or conditional statements.
+# You have to use the variables str1 and str2 in your new line of code
+# Your program should be exactly 5 lines long
+
+str1 = "Holberton"
+str2 = "School"
+print(f"Welcome to {str1} {str2}!")
