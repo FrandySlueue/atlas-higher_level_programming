0b5b2bb939e1c3f01ff29b49fc377b378ada3dc3
Frandy Slueue <frandyslueue@hotmail.com>
2024-10-11 10:03:26 -0500

    Print the Zen of Python

---
+++ python-hello_world/7-easter_egg.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+# Write a Python script that prints “The Zen of Python”, by TimPeters, followed by a new line.
+# Your script should be maximum 98 characters long (please check with wc -m 9-easter_egg.py)
+
+import this
