9c363a87cfc46a58f0e7677ee12d2e3abeb6e42d
Frandy Slueue <frandyslueue@hotmail.com>
2024-10-11 09:11:55 -0500

    Print the content of a variable using f-strings

---
+++ python-hello_world/1-print_number.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# Complete this source code (number = 98) in order to print the integer stored in the variable number, followed by Battery street, followed by a new line.
+
+# You can find the source code here
+# The output of the script should be:
+# the number, followed by Battery street,
+# followed by a new line
+# You are not allowed to cast the variable number into a string
+# Your code must be 3 lines long
+# You have to use f-strings tips
+
+number = 98
+print(f"{number} Battery street")
