1dd48a3a5bfe6f04e6fbc204935e4d3f12cd2be5
Frandy Slueue <frandyslueue@hotmail.com>
2024-10-11 09:43:43 -0500

    Manipulate a string with slicing and concatenation

---
+++ python-hello_world/6-concat_edges.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# Complete this source code (str = "Python is an interpreted, interactive, object-oriented programming\ language that combines remarkable power with very clear syntax") to print `object-oriented programming with Python`, followed by a new line.
+
+# You can find the source code here
+# You are not allowed to use any loops or conditional statements
+# Your program should be exactly 5 lines long
+# You are not allowed to create new variables
+# You are not allowed to use string literals
+
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+py = str[:6]
+obj = str[39:66]
+wit = str[-22:-18]
+print(f"{obj} {wit} {py}")
