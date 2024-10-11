2c5d1fbfe4ce03cf46162da48fbfe30268def53a
Frandy Slueue <frandyslueue@hotmail.com>
2024-10-11 09:15:46 -0500

    Print the float number stored in a variable with a precision of 2 digits using f-strings

---
+++ python-hello_world/2-print_float.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# Complete the source code (number = 3.14159) in order to print the float stored in the variable number with a precision of 2 digits.
+
+# You can find the source code here
+# The output of the program should be:
+# Float:, followed by the float with only 2 digits
+# followed by a new line
+# You are not allowed to cast number to string
+# You have to use f-strings
+
+number = 3.14159
+print(f"{number:.2f}")
