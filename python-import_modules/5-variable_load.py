69a58030baa304bfff1a0534ca1cc52b7b085742
Frandy Slueue <frandyslueue@hotmail.com>
2024-10-14 17:11:35 -0500

    Print value of import variable a

---
+++ python-import_modules/5-variable_load.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Write a program that imports the variable a from the file variable_load_5.py and prints its value.
+
+# You are not allowed to use * for importing or __import__
+# Your code should not be executed when imported
+
+from variable_load_5 import a
+
+print(a)
