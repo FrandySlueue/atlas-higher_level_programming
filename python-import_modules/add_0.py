d91d0d831e13654274f8eba8807c082a2aa01c30
Frandy Slueue <frandyslueue@hotmail.com>
2024-10-14 08:59:35 -0500

    Import add() from add_0 module and compute the sum of two integers

---
+++ python-import_modules/add_0.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def add(a: int, b: int) -> int:
+   """
+   Add two integers and return their sum.
+
+   Args:
+       a (int): The first integer.
+       b (int): The second integer.
+
+   Returns:
+       int: The sum of a + b
+   """
+   return a + b
+
+if __name__ == ('__main__'):
+   ...
