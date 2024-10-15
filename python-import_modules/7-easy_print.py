a9bb9c190d720f83a1dbb9fc82fe99e9b95c7637
Frandy Slueue <frandyslueue@hotmail.com>
2024-10-14 23:09:32 -0500

    Print to standard output

---
+++ python-import_modules/7-easy_print.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Write a program that prints #pythoniscool, followed by a new line,\
+   # in the standard output.
+
+# Your program should be maximum 2 lines long
+# You are not allowed to use print or eval or open or import \
+   # sys in your file 101-easy_print.py
+
+import os
+
+os.write(1, b"#pythonischool\n")
+"""Print to standard output with os module.
+"""
+
+if __name__ == '__main__':
+   ...
