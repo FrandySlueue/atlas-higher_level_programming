22e8578fcdbf4edbd5f51d3c7f24531ee926b864
Frandy Slueue <frandyslueue@hotmail.com>
2024-10-16 17:50:17 -0500

    Create a magic calculator using disassembler for Python bytecode, the dis module

---
+++ python-import_modules/8-magic_calculation.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+from dis import dis
+from typing import Callable
+
+def magic_calculation(a: int, b: int) -> int:
+    """
+    This function performs a magic calculation using the 'add' and 'sub' 
+    functions from the 'magic_calculation_102' module.
+
+    - If `a` is less than `b`, it calculates `c = add(a, b)`, and then adds 
+      values from 4 to 5 to `c` using the `add` function.
+    - If `a` is greater than or equal to `b`, it returns `sub(a, b)`.
+
+    Args:
+        a (int): The first integer for comparison and calculation.
+        b (int): The second integer for comparison and calculation.
+
+    Returns:
+        int: The result of the calculation, either from the `add` function or 
+             the `sub` function depending on the comparison of `a` and `b`.
+    """
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        c: int = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)  # If a >= b, return sub(a, b)
+
+
+dis(magic_calculation)
